0f59eb099b3b67768e9dbee3aaf054803598764e
Jardeylson jacinto <jardeylsong.m@gmail.com>
2023-04-09 11:08:40 -0300

    feat: created endpoint to send the description to ChatGPT

--- application/src/App.js
+++ application/src/App.js
@@ -1,50 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-async function sendDescription(){
-  try{
-    const request = await fetch('http://localhost:4000/api', {
-      method: 'POST',
+async function sendDescription() {
+  try {
+    const request = await fetch("http://localhost:4000/api", {
+      method: "POST",
       body: JSON.stringify({
         prompt: description,
       }),
       headers: {
-        accept: 'application/json',
-        'Content-Type': 'application/json',
+        Accept: "application/json",
+        "Content-Type": "application/json",
       },
     });
     const res = await request.json();
     console.log(res);
   } catch (err) {
-    console.log(err);
+    console.error(err);
   }
 }
-
 const App = () => {
   const [description, setDescription] = useState("");
   const [loading, setLoading] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
+    // calls the async function
     sendDescription();
     setDescription("");
     setLoading(true);
-  }
+  };
 
   return (
-    <div className='app'>
+    <div className="app">
       <h1>Website Idea Generator</h1>
-      <form method='POST' onSubmit={handleSubmit}>
+      <form method="POST" onSubmit={handleSubmit}>
         <label htmlFor="description">Enter the description</label>
-        <textarea 
+        <textarea
           name="description"
-          rows={6} 
-          value={description} 
-          onChange={(e) => setDescription(e.target.value)} />
-          <button>GENERATE</button>
+          rows={6}
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <button>GENERATE</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default App;
--- server/src/index.js
+++ server/src/index.js
@@ -7,6 +7,32 @@ const cors = require('cors');
 const app = express();
 const PORT = 4000;
 
+async function chatgptFunction(content) {
+  try {
+      const api = new ChatGPTAPIBrowser({
+          email: "YOUR_CHATGPT_EMAIL_ADDRESS",
+          password: "YOUR_CHATGPT_PASSWORD",
+      });
+      await api.initSession();
+
+      // sends the instruction for the domain name to ChatGPT
+      const getDomainName = await api.sendMessage(
+          `Can you generate a domain name for a website about: ${content}`
+      );
+      let domainName = getDomainName.response;
+
+      // sends the instruction for the prompt to ChatGPT
+      const generatePrompt = await api.sendMessage(
+          `I have a website for ${content}, and I want to generate a logo for it, can you generate a prompt for dall-e for me? make it long like 50 words, you don't need to tell me why you generated the prompt`
+      );
+      const diffusionPrompt = generatePrompt.response;
+
+      console.log({ domainName, diffusionPrompt });
+  } catch (err) {
+      console.error(err);
+  }
+}
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
@@ -15,9 +41,10 @@ app.get('/api', ( req, res ) => {
   res.json({ message: "Hello world", });
 });
 
-app.post('/api', async ( req, res ) => {
-  const { promp } = req.body;
-  console.log(promp);
+app.post("/api", async (req, res) => {
+  const { prompt } = req.body;
+  const result = await chatgptFunction(prompt);
+  res.json({ message: "Retrieved successfully!" });
 });
 
 app.listen(PORT, () => {
